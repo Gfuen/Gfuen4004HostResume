3b1f359b36153d982691628adf09ec8e9f6c8c02
Gregory Fuentes <gregoryfuentes80@gmail.com>
2024-01-07 21:08:13 -0600

    Add Terraform hosting files for after Cert is validated. Add resume.

---
+++ TerraformHosting/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.2.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "gfuen4004hostresume"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
---
+++ TerraformHosting/terraform.tfvars
@@ -0,0 +1,6 @@
+domain_name = "gfuen4004hostresume.com"
+bucket_name = "gfuen4004hostresume"
+
+common_tags = {
+  Project = "gfuen4004hostresume"
+}
